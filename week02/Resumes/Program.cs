ccdf53c2d2d9d432d23ba83812ee8e6e0f4c5124
Dallin Romero <dallinromero2002@gmail.com>
2025-03-12 19:32:22 -0500

    Week 02 - Resume and Job

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        // Object #1
+        Job job1 = new Job("Microsoft", "Software Engineer", 2018, 2023);
+
+        //Object #2 
+        Job job2 = new Job("Apple","Manager",2022,2023);
+
+        Resume myResume = new Resume();
+        myResume._name = "Dallin Romero";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
