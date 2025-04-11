9c97c2c36360287bf2cbad253a9b5807fe680c1f
Dallin Romero <dallinromero2002@gmail.com>
2025-04-10 19:50:08 -0500

    Shapes complete

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // elementos
+        Square cuadrado = new Square(3,"RED");
+        Rectangle rectangulo = new Rectangle(5,4,"Blue");
+        Circle circulo = new Circle(5,"Yellow");
+        
+        // Lista
+        List<Shapes> shapes = new List<Shapes>();
+        shapes.Add(cuadrado);
+        shapes.Add(rectangulo);
+        shapes.Add(circulo);
+
+        foreach (Shapes i in shapes)
+        {
+            double area = i.GetArea();
+            string c = i.GetColor();
+            Console.WriteLine($"Color: {c}, Area: {area}");
+        }
+        
+        // public static void DisplayImformation(Shapes shape, Shapes color)
+        // {
+        //     double area = shape.GetArea();
+        //     string c = color.GetColor();
+        //     Console.WriteLine($"Color: {c}, Area: {area}");
+        // }
+        
     }
 }
---
+++ week06/Shapes/Shapes.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Drawing;
+
+public abstract class Shapes
+{
+    protected string _color;
+
+    public Shapes(string color)
+    {
+        _color = color;
+    }
+
+    public string GetColor()
+    {
+        return _color;
+    }
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+
+    public abstract double GetArea();
+
+    
+
+
+} 
